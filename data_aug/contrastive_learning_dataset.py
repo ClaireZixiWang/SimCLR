a17a703d4cf67e5b21f245d9b1d87206e3e448b4
ClaireZixiWang <zixi.claire.wang@gmail.com>
2021-12-11 20:45:47 -0500

    Add Target transformer in data aug

--- data_aug/contrastive_learning_dataset.py
+++ data_aug/contrastive_learning_dataset.py
@@ -1,7 +1,7 @@
 from torchvision.transforms import transforms
 from data_aug.gaussian_blur import GaussianBlur
 from torchvision import transforms, datasets
-from data_aug.view_generator import ContrastiveLearningViewGenerator
+from data_aug.view_generator import ContrastiveLearningViewGenerator, ContrastiveLearningViewLabelGenerator
 from exceptions.exceptions import InvalidDatasetSelection
 
 
@@ -26,12 +26,16 @@ class ContrastiveLearningDataset:
                                                               transform=ContrastiveLearningViewGenerator(
                                                                   self.get_simclr_pipeline_transform(32),
                                                                   n_views),
+                                                              target_transform=ContrastiveLearningViewLabelGenerator(
+                                                                  n_views),
                                                               download=True),
 
                           'stl10': lambda: datasets.STL10(self.root_folder, split='unlabeled',
                                                           transform=ContrastiveLearningViewGenerator(
                                                               self.get_simclr_pipeline_transform(96),
                                                               n_views),
+                                                            target_transform=ContrastiveLearningViewLabelGenerator(
+                                                                  n_views),
                                                           download=True)}
 
         try:
--- data_aug/view_generator.py
+++ data_aug/view_generator.py
@@ -12,3 +12,13 @@ class ContrastiveLearningViewGenerator(object):
 
     def __call__(self, x):
         return [self.base_transform(x) for i in range(self.n_views)]
+
+
+class ContrastiveLearningViewLabelGenerator(object):
+    """Take two random crops of one image as the query and key."""
+
+    def __init__(self, n_views=2):
+        self.n_views = n_views
+
+    def __call__(self, x):
+        return [x for i in range(self.n_views)]
